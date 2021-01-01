eeb8ed3c91c0211d28cd97d6c35d3b9519fdc862
zlm <zachmurray.dev@gmail.com>
2020-12-31 20:17:48 -0600

    cleanup

--- app_proto_4.js
+++ app_proto_4.js
@@ -22,8 +22,6 @@ function decode() {
     while (position < data.length) {
       let Record = {};
 
-      // let RecordTypeEnum1 = data.slice(9, 10).readInt8();
-      // console.log('RecordTypeEnum1', RecordTypeEnum1);
       let RecordTypeEnum = data.slice(position, ++position).readInt8();
       console.log('RecordTypeEnum', RecordTypeEnum);
       switch (RecordTypeEnum) {
@@ -44,24 +42,15 @@ function decode() {
           break;
       }
 
-      // console.log('position', position)
-      // let UnixTimeStamp1 = data.slice(10, 14).readIntBE(0, 4);
-      // console.log('UnixTimeStamp1', UnixTimeStamp1);
       let UnixTimeStamp = data.slice(position, position += 4).readIntBE(0, 4);
       console.log('UnixTimeStamp', UnixTimeStamp);
-      // console.log('position', position)
 
-      // let UserId1 = data.slice(14, 22).readBigUInt64BE(0, 8);
-      // console.log('UserId1', UserId1);
       let UserId = data.slice(position, position += 8).readBigUInt64BE(0, 8);
       console.log('UserId', UserId);
-      // console.log('position', position)
 
       if ([0, 1].includes(RecordTypeEnum)) {
         let temp = data.slice(22, 30);
         console.log('temp', temp);
-        // let AmountInDollars1 = data.slice(22, 30).readFloatBE(0);
-        // console.log('AmountInDollars1', AmountInDollars1);
         let AmountInDollars = data.slice(position, position += 8).readFloatBE(0);
         console.log('AmountInDollars', AmountInDollars);
       }
